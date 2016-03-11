e2988f958dac9b49f5d370a926428cc5a821a5ea
Will <wcraft1204@gmail.com>
2016-03-11 12:25:34 -0700

    my first html table!

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+
+def get_csv(csv_path):
+    csv_file = open(csv_path, "rb")
+    #DictReader returns a list of dictionaries
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
 
 # This is going to route index.html file to the root
 @app.route("/")
 def index():
     template = "index.html"
-    return render_template(template)
+    object_list = get_csv("./static/la-riots-deaths.csv")
+    return render_template(template, object_list = object_list)
 
 if __name__ == "__main__":
     # fires up the flask test server
